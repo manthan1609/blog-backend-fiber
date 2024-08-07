c0211fc9c6d6a138e0577d25f1ccc6278537c844
Manthan Sharma <manthan0908sharma@gmail.com>
2024-08-07 13:36:44 +0530

    add update & delete blog logic

--- controllers/blog.go
+++ controllers/blog.go
@@ -60,11 +60,41 @@ func BlogCreate(c *fiber.Ctx) error {
 
 // Update a Blog
 func BlogUpdate(c *fiber.Ctx) error {
-	log.Println(c.Params("id"))
-
 	context := fiber.Map{
 		"statusText": "Ok",
 		"msg":        "Blog Updated",
+	}
+
+	id := c.Params("id")
+
+	var record models.Blog
+
+	database.DBConnection.Find(&record, id)
+
+	if record.ID == 0 {
+		log.Println("invalid blog id")
+		context["statusText"] = "error"
+		context["msg"] = "blog id is not valid"
+		c.Status(404)
+		return c.JSON(context)
+	}
+
+	if err := c.BodyParser(&record); err != nil {
+		log.Println("error in parsing request")
+		c.Status(400)
+		context["statusText"] = "failed"
+		context["msg"] = "error in parsing request"
+		return c.JSON(context)
+	}
+
+	result := database.DBConnection.Save(record)
+
+	if result.Error != nil {
+		log.Println("database error in updating blog")
+		c.Status(500)
+		context["statusText"] = "failed"
+		context["msg"] = "database error while updating blog"
+		return c.JSON(context)
 	}
 
 	c.Status(200)
@@ -77,6 +107,31 @@ func BlogDelete(c *fiber.Ctx) error {
 		"statusText": "Ok",
 		"msg":        "Blog Deleted",
 	}
+
+	id := c.Params("id")
+
+	var record models.Blog
+
+	database.DBConnection.Find(&record, id)
+
+	if record.ID == 0 {
+		log.Println("invalid blog id")
+		context["statusText"] = "error"
+		context["msg"] = "blog id is not valid"
+		c.Status(404)
+		return c.JSON(context)
+	}
+
+	result := database.DBConnection.Delete(record, id)
+
+	if result.Error != nil {
+		log.Println("database error in deleting blog")
+		c.Status(500)
+		context["statusText"] = "failed"
+		context["msg"] = "database error while deleting blog"
+		return c.JSON(context)
+	}
+
 	c.Status(200)
 	return c.JSON(context)
 }
